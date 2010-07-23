b07c2ae272cdf9b215d87dd5d582a42eb389164d
Sam Graham <git@illusori.co.uk>
2010-07-23 12:03:58 +0000

    Initial revision

---
+++ src/lib/Task/Template/Benchmark.pm
@@ -0,0 +1,110 @@
+package Task::Template::Benchmark;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Task::Template::Benchmark - The great new Task::Template::Benchmark!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Task::Template::Benchmark;
+
+    my $foo = Task::Template::Benchmark->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Sam Graham, C<< <perl-modules at illusori.co.uk> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-task-template-benchmark at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Task-Template-Benchmark>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Task::Template::Benchmark
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Task-Template-Benchmark>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Task-Template-Benchmark>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Task-Template-Benchmark>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Task-Template-Benchmark/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2010 Sam Graham.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Task::Template::Benchmark
